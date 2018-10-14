9a3e53a847b6e98425a4f41fe985711db376a2a0
Uy Binh <uybinh@gmail.com>
2018-10-14 22:49:46 +0700

    Add exercise model

---
+++ model/exercise.js
@@ -0,0 +1,23 @@
+const shortid = require("shortid")
+const mongoose = require("mongoose")
+const schema = mongoose.Schema({
+	_id: {
+		type: String,
+		default: shortid.generate,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	duration: {
+		type: String,
+		required: true
+	},
+	date: {
+		type: Date,
+		required: true
+	}
+})
+
+module.exports = mongoose.model("Exercise", schema)
